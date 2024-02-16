9419e5159e1a6aa2c53dcce143da97c955d56cb7
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-02-16 09:29:53 +0100

    add: action pour l'inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -22,6 +28,44 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        /* test de connexion pour la partie front
+        $user = $this->getUser();
+        return $this->json(['user' => $user, 'error' => $error]);
+        */
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user, [
+            'attr' => ['class' => 'register-form']
+        ])->add('register', SubmitType::class, ['label' => "S'inscrire", 'attr' => ['class' => 'btn button-primary full-width']]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // vérification si un utilisateur a déjà l'email saisi
+            $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+
+            if ($existingUser) {
+                $this->addFlash('error', 'Cet email est déjà associé à un compte.');
+            } else {
+                $password = $form->get('password')->getData();
+                $user->setPassword($userPasswordHasher->hashPassword($user, $password));
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Votre compte a bien été créé!');
+                return $this->redirectToRoute('app_login');
+            }
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
