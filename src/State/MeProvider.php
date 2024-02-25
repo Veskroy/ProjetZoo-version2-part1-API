6b0b625f85724b081df8ea7795ee08a26c569bd0
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-02-25 20:14:29 +0100

    add: provider pour l'utilisateur courant

---
+++ src/State/MeProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class MeProvider implements ProviderInterface
+{
+    public User $user;
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        // Retrieve the state from somewhere
+        return $this->user;
+    }
+
+}
