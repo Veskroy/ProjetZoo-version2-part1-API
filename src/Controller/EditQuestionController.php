605d03c48b2bf0547a36fd30af5d2e8daf6c02c8
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-02-26 20:43:16 +0100

    add: controller pour la modification d'une question

---
+++ src/Controller/EditQuestionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Question;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EditQuestionController extends AbstractController
+{
+    public function __invoke(Question $data): Question
+    {
+        $data->setUpdatedAt(new \DateTimeImmutable());
+        return $data;
+    }
+}
