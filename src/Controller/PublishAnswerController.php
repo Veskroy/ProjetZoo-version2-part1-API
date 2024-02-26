ec6bb5cde1b0663c93a7b8577eac9f68f70299b1
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-02-26 20:42:59 +0100

    add: controller pour la publication d'une question

---
+++ src/Controller/PublishAnswerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Answer;
+use App\Entity\Question;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PublishAnswerController extends AbstractController
+{
+    public function __invoke(Answer $data, Question $question): Answer
+    {
+        $data->setAuthor($this->getUser());
+        $data->setCreatedAt(new \DateTimeImmutable());
+        $data->setQuestion($question);
+        return $data;
+    }
+}
