0526282b6ea9a3417f7c2ccb249766f93af451d1
Vincent <vincent.kpatinde@etudiant.univ-reims.fr>
2024-03-07 19:15:44 +0100

    Add: Filtre Spot.php

--- src/Entity/Spot.php
+++ src/Entity/Spot.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\SpotRepository;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -9,6 +12,8 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SpotRepository::class)]
+#[ApiFilter(OrderFilter::class, properties:['id','name','pens'], arguments: ['orderParameterName' => 'order'])]
+#[ApiFilter(SearchFilter::class, properties: ['id'=>'exact','name'=>'partial','pens'=>'exact'])]
 #[ApiResource]
 class Spot
 {
