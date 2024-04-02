71ca47939b6289a261ed02bdb5a1a3a0e1717626
jaco0024 <logan.jacotin@etudiant.univ-reims.fr>
2024-04-03 01:17:52 +0200

    Add: Entity Manager dans le construct

--- src/Controller/EnclosuresWithAnimalsController.php
+++ src/Controller/EnclosuresWithAnimalsController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Pen;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 class EnclosuresWithAnimalsController  extends AbstractController
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
 
 }
