e439f21c3026e8eaf3ca23ba1f54633ffdca441f
jaco0024 <logan.jacotin@etudiant.univ-reims.fr>
2024-04-03 21:33:09 +0200

    Add: EditTicketController

---
+++ src/Controller/EditTicketController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ticket;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EditTicketController extends AbstractController
+{
+
+}
