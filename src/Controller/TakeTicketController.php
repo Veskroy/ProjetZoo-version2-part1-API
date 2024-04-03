f9c5bbc3866fac45deaa15d544e4d7880e728014
jaco0024 <logan.jacotin@etudiant.univ-reims.fr>
2024-04-03 22:14:18 +0200

    Add: TakeTicketController

--- src/Controller/TakeTicketController.php
+++ src/Controller/TakeTicketController.php
@@ -9,5 +9,13 @@ use Symfony\Bundle\SecurityBundle\Security;
 
 class TakeTicketController extends AbstractController
 {
+    public function __invoke(Security $security, EntityManagerInterface $entityManager, Ticket $data): Ticket
+    {
+        $data->setUser($security->getUser());
+        $data->setDate(new \DateTimeImmutable());
+        $entityManager->persist($data);
+        $entityManager->flush();
 
+        return $data;
+    }
 }
